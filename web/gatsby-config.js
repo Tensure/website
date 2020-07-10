7c809c4fab345306fa11bc99ef3e9196238c89b0
Chris Costanza <chris@mindboxstudios.com>
2020-07-10 18:53:01 -0400

    getting fonts to load faster.

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,24 +9,24 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   plugins: [
+    // {
+    //   resolve: `gatsby-plugin-web-font-loader`,
+    //   options: {
+    //     typekit: {
+    //       id: `kic0ucd`
+    //     }
+    //   }
+    // },
     `gatsby-plugin-preload-fonts`,
-    'gatsby-plugin-postcss',
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: `gatsby-source-sanity`,
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
         watchMode: !isProd,
         overlayDrafts: !isProd
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        typekit: {
-          id: 'kic0ucd'
-        }
       }
     },
     {
@@ -43,23 +43,11 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-plugin-offline',
+      resolve: `gatsby-plugin-offline`,
       options: {
          workboxConfig: {
-            globPatterns: ['**/*']
+            globPatterns: [`**/*`]
          }
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-netlify',
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       }
     }
   ]
