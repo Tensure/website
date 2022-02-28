dbc80c69b1a33fc7a334ab08ca40375b3615ae4c
Sam Hollyer <rsamhollyer@gmail.com>
2022-02-28 09:10:02 -0600

    Add customer success component and styles

---
+++ web/src/components/customer-success/CustomerStories.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import BlogPostPreview from '../blog-post-preview'
+import styles from './customer-success.module.css'
+
+export default function CustomerStories (props) {
+  return (
+    <div className={styles.root}>
+      <ul className={styles.grid}>
+        {props.nodes &&
+          props.nodes.slice(0, 3).map((node) => (
+            <li key={node.id} datacat={node.categories[0].title}>
+              <BlogPostPreview {...node} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
+
+CustomerStories.defaultProps = {
+  nodes: []
+}
