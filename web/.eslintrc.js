9e6469118fc6d51010aaac834052ea6142462f6d
Sam Hollyer <rsamhollyer@gmail.com>
2022-03-01 11:10:04 -0600

    remove eslint uipdate

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ["standard", "standard-react", "plugin:import/errors", "plugin:import/warnings"],
-  ignorePatterns: ["node_modules/"],
   rules: {
     "react/prop-types": 0,
     "object-curly-spacing": ["error", "never"],
